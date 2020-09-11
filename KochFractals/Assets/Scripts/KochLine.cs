242c5d02b65330126246f83d5da38c796679f9f1
Iseta <isabelle.brandelero@gmail.com>
2020-09-11 14:51:57 -0300

    Commit inicial, aula 3

---
+++ KochFractals/Assets/Scripts/KochLine.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class KochLine : KochGenerator
+{
+    LineRenderer _lineRenderer;
+    [Range(0,1)]
+    public float lerpAmount;
+    public float generateMultiplier;
+    Vector3[] _lerpPosition;
+    void Start()
+    {
+        _lineRenderer = GetComponent<LineRenderer>();
+        _lineRenderer.enabled = true;
+        _lineRenderer.useWorldSpace = false;
+        _lineRenderer.loop = true;
+        _lineRenderer.positionCount = _position.Length;
+        _lineRenderer.SetPositions(_position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(_generationCount != 0)
+        {
+            for(int i = 0; i < _position.Length; i++)
+            {
+                _lerpPosition[i] = Vector3.Lerp(_position[i], _targetPosition[i], lerpAmount);
+            }
+            _lineRenderer.SetPositions(_lerpPosition);
+        }
+
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            GenerateKoch(_targetPosition, true, generateMultiplier);
+            _lerpPosition = new Vector3[_position.Length];
+            _lineRenderer.positionCount = _position.Length;
+            _lineRenderer.SetPositions(_position);
+            lerpAmount = 0;
+        }
+
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            GenerateKoch(_targetPosition, false, generateMultiplier);
+            _lerpPosition = new Vector3[_position.Length];
+            _lineRenderer.positionCount = _position.Length;
+            _lineRenderer.SetPositions(_position);
+            lerpAmount = 0;
+        }
+    }
+}
